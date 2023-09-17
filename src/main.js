6028999d83e3b342f94549b99c90c141f5629fe3
Manuel Takata <takata211011@gmail.com>
2023-09-17 14:40:50 -0500

    add post

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import express from 'npm:express@^4.17'
+import express from 'npm:express@4.17'
+import cors from 'npm:cors'
 const app = express()
-app.listen(3000)
-console.log('hola')
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+const PORT = Deno.env.get('PORT') || 4000
+app.get('/', (req, res) => {
+  res.send('Hello TKT')
+})
+app.post('/', async (req, res) => {
+  const { url } = req.body
+  try {
+    console.log('ephemeris')
+    const response = await fetch(url, {
+      method: 'POST',
+    })
+    const data = await response.json()
+    console.log(data)
+    res.json(data)
+  } catch (error) {
+    console.log(error)
+  }
+})
+app.listen(PORT)
